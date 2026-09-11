157f9d963f71fe1e744ce94d0844a48795e16958
Sylwester Lachiewicz <slachiewicz@apache.org>
2026-09-11 23:41:44 +0200

    Site checkin for project JDepend Maven Plugin

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.mojo.jdepend"},{"l":"org.codehaus.mojo.jdepend_maven_plugin"},{"l":"org.codehaus.mojo.jdepend.objects"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.codehaus.mojo.jdepend"},{"l":"org.codehaus.mojo.jdepend_maven_plugin"},{"l":"org.codehaus.mojo.jdepend.objects"}];updateSearchResults();
